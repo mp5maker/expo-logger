3be81765f1a08423cf5b8f7d8e113000ffa413b8
mp5maker <khan.photon@gmail.com>
2022-09-15 13:34:43 +0600

    :gear: chore: updated the git ignore

--- src/abstract/index.ts
+++ src/abstract/index.ts
@@ -1,21 +1,21 @@
-import { PRIMITIVE_TYPES } from '../constants'
-import { TReplacer } from '../interface'
+import { PRIMITIVE_TYPES } from "../constants";
+import { TReplacer } from "../interface";
 
 export abstract class LoggerAbstract {
-  protected replacer?: TReplacer
-  protected spacing?: number
+  protected replacer?: TReplacer;
+  protected spacing?: number;
 
   constructor(replacer = undefined, spacing = 4) {
-    this.replacer = replacer
-    this.spacing = spacing
+    this.replacer = replacer;
+    this.spacing = spacing;
   }
 
   conversion(args: Array<any>): Array<any> {
-    return args.map(item => {
-      if (PRIMITIVE_TYPES.includes(typeof item)) return item
-      return JSON.stringify(item, this.replacer, this.spacing)
-    })
+    return args.map((item) => {
+      if (PRIMITIVE_TYPES.some((item) => item === typeof item)) return item;
+      return JSON.stringify(item, this.replacer, this.spacing);
+    });
   }
 
-  abstract performAction(argumentList: Array<any>): void
+  abstract performAction(argumentList: Array<any>): void;
 }
